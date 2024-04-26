67dac56b860c213072d7777a12490f43a9c06d00
Bullets-jyr <jyr@bullets.co.kr>
2024-04-26 21:49:05 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/kr/co/bullets/tiktok_clone/MainActivity.kt
@@ -0,0 +1,5 @@
+package kr.co.bullets.tiktok_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Container(),
+    );
+  }
+}
